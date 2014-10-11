d7f23ef0168a487cc3afaa5886c14735ed7a11f2
Justin Puksic <justin994@web.de>
2014-10-12 01:45:47 +0200

    Create Gameobject.java

---
+++ Gameobject.java
@@ -0,0 +1,29 @@
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.*;
+
+public abstract class Gameobject {
+
+	protected int x_Pos;
+	protected int y_Pos;
+	protected Image gfx_char;
+	protected Shape hitbox;
+	
+
+	public Gameobject(int x_Pos, int y_Pos) {
+		this.x_Pos = x_Pos;
+		this.y_Pos = y_Pos;
+	}
+
+	// Getter & Setter
+	public Shape getHitbox() {
+		return hitbox;
+	}
+
+	public void setHitbox(Shape hitbox) {
+		this.hitbox = hitbox;
+	}
+
+	public abstract void draw();
+
+}
