5dad21e70dce7971298c93f957dc9668c3744598
Justin Puksic <justin994@web.de>
2014-10-25 00:21:23 +0200

    Update Spiel.java
    outsourced and added functions like a small highscore

--- Spiel.java
+++ Spiel.java
@@ -1,65 +1,71 @@
-
-
 import java.util.LinkedList;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.tiled.TiledMap;
 
 public class Spiel extends BasicGame {
 
 	private Image level;
 	private Player player;
 	private Input steuerung;
-	private Monster[] monster;
+	protected LinkedList<Monster> monster_anzahl;
 	protected LinkedList<Schuss> schuss_anzahl;
+	protected Pawn pawn;
 	private Music music;
 	private Sound sound;
-	private int cooldown;
+	private Score highscore;
+	@SuppressWarnings("unused")
+	private TiledMap map;
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
 		level = new Image("hintergrund.png");
-		monster = new Monster[10];
 		steuerung = gc.getInput();
 		player = new Player(740, 395, steuerung, "ghost.png");
-		for (int i = 0; i < monster.length; i++) {
-			monster[i] = new Monster(400 + (i * 50), 428, "goomba.png");
-		}
 		schuss_anzahl = new LinkedList<Schuss>();
+		monster_anzahl = new LinkedList<Monster>();
+		pawn = new Pawn();
 		music = new Music("kirby.ogg");
 		music.loop();
-		music.setVolume(0.5f);
+		music.setVolume(0.0f);
 		sound = new Sound("gun.wav");
+		highscore = new Score();
+		// map = new TiledMap("map.tmx");
+		for (int i = 0; i < 5; i++) {
+			monster_anzahl.add(new Monster(400 + (i * 50), 28, "goomba.png"));
+		}
 	}
 
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
 
-		cooldown += delta;
-
 		player.update();
-
-		for (int i = 0; i < monster.length; i++) {
-			monster[i].laufen(3);
-
-		}
-
 		player.laufen(3, delta);
+		if (!(monster_anzahl.isEmpty())) {
 
-		if (steuerung.isMousePressed(Input.MOUSE_LEFT_BUTTON)
-				&& cooldown < 10000) {
-			schuss_anzahl.add(new Schuss(player.x_Pos + 40, player.y_Pos,
-					"Schuss.png"));
-			sound.play();
-
-		}
-
-		// Schuss wird nur dann bewegt wenn das Objekt existiert
-		if (!(schuss_anzahl.isEmpty())) {
-			for (int i = 0; i < schuss_anzahl.size(); i++) {
-				if (!(schuss_anzahl.get(i).y_Pos < 0)) {
-					schuss_anzahl.get(i).laufen(5, schuss_anzahl);
+			for (int i = 0; i < monster_anzahl.size(); i++) {
+				if (!(monster_anzahl.get(i).y_Pos < 0)) {
+					monster_anzahl.get(i).laufen(2);
 				}
 			}
+			if (steuerung.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+				schuss_anzahl.add(new Schuss(player.x_Pos + 10, player.y_Pos,
+						"Schuss.png"));
+				sound.play();
+			}
+
+			// Schuss wird nur dann bewegt wenn das Objekt existiert
+			if (!(schuss_anzahl.isEmpty())) {
+				for (int i = 0; i < schuss_anzahl.size(); i++) {
+					if (!(schuss_anzahl.get(i).y_Pos < 0)) {
+						schuss_anzahl.get(i).laufen(5);
+						pawn.check_Col(schuss_anzahl, monster_anzahl, highscore);
+
+					}
+				}
+
+			}
+
 		}
 
 	}
@@ -68,21 +74,18 @@ public class Spiel extends BasicGame {
 	public void render(GameContainer gc, Graphics gfx) throws SlickException {
 		gfx.setColor(Color.magenta);
 		level.draw(0, 0);
-
-		gfx.drawString("Zeit: " + cooldown, 10, 50);
-
-		for (int i = 0; i < monster.length; i++) {
-			monster[i].draw();
-			gfx.draw(monster[i].getHitbox());
-			if (monster[i].getHitbox().intersects(player.getHitbox())) {
-				gfx.drawString("Treffer", 10, 30);
-			}
-		}
+        gfx.drawString("Punkte: " + highscore.getScore(),10,70);
 		player.draw();
 		if (!(schuss_anzahl.isEmpty())) {
 			for (int i = 0; i < schuss_anzahl.size(); i++) {
 				schuss_anzahl.get(i).draw();
 				gfx.draw(schuss_anzahl.get(i).getHitbox());
+			}
+		}
+		if (!(monster_anzahl.isEmpty())) {
+			for (int i = 0; i < monster_anzahl.size(); i++) {
+				monster_anzahl.get(i).draw();
+				gfx.draw(monster_anzahl.get(i).getHitbox());
 			}
 		}
 		gfx.draw(player.getHitbox());
@@ -100,7 +103,6 @@ public class Spiel extends BasicGame {
 			game.setTargetFrameRate(60);
 			game.setVSync(true);
 			game.setIcon("icon.png");
-			game.setAlwaysRender(true);
 			game.setMouseGrabbed(false);
 			game.setShowFPS(true);
 			game.start();
