baf5da361d790c3127659381579e24a562a6c2d7
Justin Puksic <justin994@web.de>
2014-10-25 00:21:56 +0200

    Create Score.java

---
+++ Score.java
@@ -0,0 +1,32 @@
+package test;
+
+public class Score {
+	
+	private int score;
+	private int monster_punkte;
+
+	public Score() {
+		
+		this.setScore(0);
+		this.monster_punkte = 5;
+		
+		
+
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public int getMonster_punkte() {
+		return monster_punkte;
+	}
+
+	
+
+
+}
